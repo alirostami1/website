6ba160c8ea652468a3a54f91953a695af0547b25
Ali Rostami <ali.rostami@live.com>
2024-05-08 22:52:35 +0330

    first commit

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+    const blog = await getCollection('blog');
+    return rss({
+        title: 'Ali Rostami\'s Blog',
+        description: 'A Programmer Prespective of the World',
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            author: 'Ali Rostami',
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            customData: post.data.customData,
+            link: `/blog/${post.slug}/`,
+        })),
+    });
+}
