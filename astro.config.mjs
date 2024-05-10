181ae51cf177c8b46b704d78daa9920ba95c703b
Ali Rostami <ali.rostami@live.com>
2024-05-10 16:23:08 +0330

    removed empty line

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import icon from "astro-icon";
 
 // https://astro.build/config
